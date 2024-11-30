60e8a2266e4585ccf2859465ff0a4dae9b3aae63
Ana Chiachio <kessilly_chiachio@hotmail.com>
2024-11-30 10:54:20 -0300

    json for js

---
+++ collections/oficinas.js
@@ -0,0 +1,27 @@
+use asvic;
+
+db.oficinas.insertMany(
+  [
+    {
+      "oficina_credenciada" : "Oficina A",
+      "telefone_whatsapp" : "11987654321"
+    },
+    {
+      "oficina_credenciada" : "Oficina B",
+      "telefone_whatsapp" : "11976543210"
+    },
+    {
+      "oficina_credenciada" : "Oficina C",
+      "telefone_whatsapp" : "11965432109"
+    },
+    {
+      "oficina_credenciada" : "Oficina D",
+      "telefone_whatsapp" : "11954321098"
+    },
+    {
+      "oficina_credenciada" : "Oficina E",
+      "telefone_whatsapp" : "11943210987"
+    }
+  ]
+  
+)
